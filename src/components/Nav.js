7b04083298c07917124d45aa41b80bca0733a56f
Fozle Rabbi Shafi <frshafi49@gmail.com>
2022-06-06 04:17:46 +0600

    updated logo and delete unnecesary files

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logo from '../assets/images/logo-bg.png'
+import logo from '../assets/images/logo.svg'
 import classes from '../styles/Nav.module.css'
 import Account from './Account'
 
@@ -10,7 +10,7 @@ export default function Nav() {
                 <li>
                     <Link to="/" className={classes.brand}>
                         <img src={logo} alt="Learn with Sumit Logo" />
-                        <h3>Learn with Sumit</h3>
+                        <h3>Vid Quiz</h3>
                     </Link>
                 </li>
             </ul>
